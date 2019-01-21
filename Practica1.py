c2339a36345e05118e74d5be7edfaaa49397b315
RuthSN <46898238+RuthSN@users.noreply.github.com>
2019-01-21 09:42:36 -0600

    Add files via upload

---
+++ Practica1.py
@@ -0,0 +1,8 @@
+c1 = int(input("Introduce tu primera calificación: "))
+c2 = int(input("Introduce tu segunda calificación: "))
+c3 = int(input("Introduce tu tercer calificación: "))
+c4 = int(input("Introduce tu cuarta calificación: "))
+
+cf = c1*0.4 + c2*0.2 + c3*0.1 + c4*0.3
+
+print(cf)
